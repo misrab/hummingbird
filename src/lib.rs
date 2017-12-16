0fb6eabeaf9b24e8e4228cc68bcfc424f4f49bb6
misrab <faizullah.misrab@gmail.com>
2017-12-16 14:59:17 +0800

    commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+
+
+
+pub mod pdfer;
+
+
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,29 @@
+#![feature(test)]
+
+
+//extern crate test;
+
+use pdfer;
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  //use test::Bencher;
+
+
+
+  #[test]
+  fn test_test() {
+    println!("tests are running");
+  }
+
+
+
+  #[test]
+  fn test_basic() {
+    println!("test basic");
+
+  }
+
+}
